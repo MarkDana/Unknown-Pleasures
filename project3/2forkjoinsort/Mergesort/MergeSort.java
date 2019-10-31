b9a6855e23cdb3139e9b950fa6bc8e93db12f997
MarkDana <dhy326@gmail.com>
2019-10-31 19:45:29 +0800

    debugging

--- project3/2forkjoinsort/Mergesort/MergeSort.java
+++ project3/2forkjoinsort/Mergesort/MergeSort.java
@@ -53,18 +53,22 @@ public class MergeSort extends RecursiveAction {
     public static void main(String[] args) {
 
         System.out.println(new File("."));
-        // Scanner scanner = new Scanner(new File("./array.txt"));
-        // int [] array = new int [1000];
-        // int i = 0;
-        // while(scanner.hasNextInt())array[i++] = scanner.nextInt();
+        
+        try{
+        Scanner scanner = new Scanner(new File("./array.txt"));
+        int [] array = new int [1000];
+        int i = 0;
+        while(scanner.hasNextInt())array[i++] = scanner.nextInt();
+        }catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
 
 
+        // int[] array = new int[20];
 
-        int[] array = new int[20];
-
-        for (int i = 0; i < 20; i++) {
-            array[i] = new Random().nextInt(100);
-        }
+        // for (int i = 0; i < 20; i++) {
+        //     array[i] = new Random().nextInt(100);
+        // }
 
         System.out.println(Arrays.toString(array));
 
