eea85c1469b464f700db7c65f4de43dd03af9dc5
MarkDana <dhy326@gmail.com>
2019-10-31 18:36:17 +0800

    debugging

--- project3/2forkjoinsort/Mergesort/MergeSort.java
+++ project3/2forkjoinsort/Mergesort/MergeSort.java
@@ -67,41 +67,41 @@ public class MergeSort extends RecursiveAction {
 }
 
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.*;
+// import java.util.Arrays;
+// import java.util.Random;
+// import java.util.concurrent.*;
 
-class Main extends RecursiveAction {
-    private static final int SERIAL_THRESHOLD = 100;
-    private int array[];
-    private int left;
-    private int right;
+// class Main extends RecursiveAction {
+//     private static final int SERIAL_THRESHOLD = 100;
+//     private int array[];
+//     private int left;
+//     private int right;
 
-    public Main(int[] array, int left, int right) {
-        this.array = array;
-        this.left = left;
-        this.right = right;
-    }
+//     public Main(int[] array, int left, int right) {
+//         this.array = array;
+//         this.left = left;
+//         this.right = right;
+//     }
 
-    *
-     * Inherited from RecursiveAction.
-     * Compare it with the run method of a Thread.
+//     *
+//      * Inherited from RecursiveAction.
+//      * Compare it with the run method of a Thread.
      
-    @Override
-    protected void compute() {
-        if (left < right) {
-            if (serialThresholdMet()) {
-                Arrays.sort(array, left, right + 1);
-            }
-            else{
-                int mid = (left + right) / 2;
-                Main leftSort = new Main(array, left, mid);
-                Main rightSort = new Main(array, mid + 1, right);
-                invokeAll(leftSort, rightSort);
-                merge(left, mid, right);
-            }
-        }
-    }
+//     @Override
+//     protected void compute() {
+//         if (left < right) {
+//             if (serialThresholdMet()) {
+//                 Arrays.sort(array, left, right + 1);
+//             }
+//             else{
+//                 int mid = (left + right) / 2;
+//                 Main leftSort = new Main(array, left, mid);
+//                 Main rightSort = new Main(array, mid + 1, right);
+//                 invokeAll(leftSort, rightSort);
+//                 merge(left, mid, right);
+//             }
+//         }
+//     }
 
 //     private boolean serialThresholdMet() {
 //         return right - left < SERIAL_THRESHOLD;
