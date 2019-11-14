d5161147416b60fb0032f0744222a797bfb3351c
MarkDana <dhy326@gmail.com>
2019-11-14 10:21:28 +0800

    debugging

--- project4/schedule_priority.c
+++ project4/schedule_priority.c
@@ -5,33 +5,6 @@
 #include "task.h"
 #include "cpu.h"
 
-//maintain two lists to store tasks
-struct node* waitingHead = NULL;
-struct node* finishedHead = NULL;
-Task* curTask = NULL;
-
-int curTime = 0;
-int runTime = 0; //for the curTask
-
-int turnaroundTime = 0;
-int waitingTime = 0;
-int responseTime = 0;
-
-int value = 1;  //used for tid
-int numTasks = 0;  //
-
-void add(char *name, int priority, int burst){
-    Task* newtask = malloc(sizeof(Task));
-    newtask->tid = __sync_fetch_and_add(&value,1);
-    //atomically increment an integer value, atomic integer, to fix race condition
-
-    newtask->name = name;
-    newtask->priority = priority;
-    newtask->burst = burst;
-    newtask->lastOut = 0;
-    insert(&waitingHead,newtask);
-}
-
 Task* nextTask(){
     struct node* nextNode = waitingHead;
     struct node* priorNode = waitingHead; 
@@ -43,40 +16,7 @@ Task* nextTask(){
     return priorNode->task;
 }
 
-void updateTask(){
-    curTask->burst -= runTime;
-    if(curTask->burst>0){
-        curTask->lastOut = curTime;
-        delete(&waitingHead,curTask);
-        insert(&waitingHead,curTask);
-    }
-    else{
-        delete(&waitingHead,curTask);
-        insert(&finishedHead,curTask);
-        turnaroundTime += curTime;
-    }
-}
-
 
 void schedule(){
-    while (1){
-        curTask = nextTask();
-        if(curTask == NULL)break;
-        waitingTime += (curTime - curTask->lastOut);
-        if(curTask->lastOut == 0)responseTime += curTime;
-        runTime = curTask->burst;
-        run(curTask, runTime);
-        for (int t=0;t<runTime;++t) ++curTime;
-        updateTask();
-    }
-
-    struct node* fns = finishedHead;
-    while(fns != NULL){
-        ++numTasks;
-        fns = fns->next;
-    }
-    printf("\n");
-    printf("Average turnaround time: %fs\n",(float)turnaroundTime/numTasks);
-    printf("Average waiting time: %fs\n",(float)waitingTime/numTasks);
-    printf("Average response time: %fs\n",(float)responseTime/numTasks);
+    schedule_is_rr(0);
 }
