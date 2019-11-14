5238e9a332bdac1e9512be1044afe6ad2f190f59
MarkDana <dhy326@gmail.com>
2019-11-14 08:59:55 +0800

    debugging

--- project4/schedule_sjf.c
+++ project4/schedule_sjf.c
@@ -36,8 +36,8 @@ Task* nextTask(){  //fcfs, from tail
     struct node* nextNode = waitingHead;
     struct node* minNode = waitingHead; 
     if (nextNode == NULL)return NULL; 
-    while (nextNode->next != NULL){
-        if (nextNode->next->task->burst < minNode->task->burst)minNode = nextNode->next;
+    while (nextNode->next != NULL){ //<=, if times equal, pick the first come
+        if (nextNode->next->task->burst <= minNode->task->burst)minNode = nextNode->next;
         nextNode = nextNode->next;
     }
     return minNode->task;
