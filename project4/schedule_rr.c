c2bcd0e9ac20eb25daf10c5acfe77b588b09ce57
MarkDana <dhy326@gmail.com>
2019-11-14 09:23:25 +0800

    debugging

--- project4/schedule_rr.c
+++ project4/schedule_rr.c
@@ -62,7 +62,7 @@ void schedule(){
         if(curTask == NULL)break;
         waitingTime += (curTime - curTask->lastOut);
         if(curTask->lastOut == 0)responseTime += curTime;
-        runTime = min(QUANTUM,curTask)->burst;
+        runTime = min(QUANTUM,curTask->burst);
         run(curTask, runTime);
         for (int t=0;t<runTime;++t) ++curTime;
         updateTask();
