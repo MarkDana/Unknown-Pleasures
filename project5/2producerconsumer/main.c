ae9a03b63a57afdcdea556a30194ccc67b59dde8
MarkDana <dhy326@gmail.com>
2019-11-14 21:16:25 +0800

    debugging

--- project5/2producerconsumer/main.c
+++ project5/2producerconsumer/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
     int sleep_time = atoi(argv[1]);
     int nump = atoi(argv[2]);
     int numc = atoi(argv[3]);
-    pool_init(int nump, int numc);
+    pool_init(nump, numc);
     usleep(sleep_time);
     pool_shutdown();
     return 0;
