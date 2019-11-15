5fd3cd8db536f4b7a3f8646d4480e2dfea0c918c
MarkDana <dhy326@gmail.com>
2019-11-15 13:23:24 +0800

    debugging

--- project5/2producerconsumer/main.c
+++ project5/2producerconsumer/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "buffer.h" 
 
-int main(int argc, char *argv[]){   
+int main(int argc, char *argv[]){
+	if (strcmp(argv[1],"-h")==0){
+		printf("command like ./example 10 5 10  (10s, 5 producers, 10 consumers)\n");
+		return 0;}  
     int sleep_time = atoi(argv[1]);
     int nump = atoi(argv[2]);
     int numc = atoi(argv[3]);
